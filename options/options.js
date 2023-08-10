954661e9f75763f1b028b466b9af2ec719d38009
Crowdbotics <team@crowdbotics.com>
2023-08-10 00:02:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-disk-42759",
+  url: "https://soft-disk-42759.botics.co",
+  api: "https://soft-disk-42759.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
